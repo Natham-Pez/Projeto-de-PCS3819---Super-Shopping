ed4b241638b786b637a99852581c04b7c55bcde4
Felipe Soria Golghetto <felipesoriagolghetto@usp.br>
2026-04-11 19:29:23 -0300

    adição de permissão para api

--- superShopping/vite.config.ts
+++ superShopping/vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    proxy: {
+      '/analytics': {
+        target: 'http://143.107.102.8:8090',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/lab': {
+        target: 'http://143.107.102.8:8090',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
